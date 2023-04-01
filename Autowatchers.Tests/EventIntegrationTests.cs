17b453927370be3d506bb946290bc86b9cf5482e
Hernan <h.f.alvarez.rubio@gmail.com>
2023-04-01 01:48:31 -0300

    FIX: Se eliminaron tests

--- Autowatchers.Tests/EventIntegrationTests.cs
+++ Autowatchers.Tests/EventIntegrationTests.cs
@@ -36,33 +36,6 @@ public class EventIntegrationTests
     }
 
     [Fact]
-    public void EventIntegration_ShouldNotCallLogger_WhenPropertyGetSetStringIsModified_DetailIsDeep()
-    {
-        // Arrange
-        var logger = Mock.Of<ILogger<Net7.DeepWatchers.WithBlockNamespace>>();
-        var loggerMock = Mock.Get(logger);
-
-        using var @object = new Net7.DeepWatchers.WithBlockNamespace(logger);
-
-        //Act
-        @object.Test1ThatDoNotModifies();
-
-        // Assert
-        Assert.Equal(Net7.DeepWatchers.WithBlockNamespace.OldName, @object.DummyClass.GetSetString);
-        Assert.Equal(Net7.DeepWatchers.WithBlockNamespace.OldNestedValue, @object.DummyClass.NestedClass);
-
-        loggerMock.Verify(
-            x => x.Log(
-                It.IsAny<LogLevel>(),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((v, t) => true),
-                It.IsAny<Exception?>(),
-                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), Times.Never);
-
-
-    }
-
-    [Fact]
     public void EventIntegration_ShouldCallLogger_WhenPropertyNestedClassIsModified_DetailIsDeep()
     {
         // Arrange
@@ -92,32 +65,6 @@ public class EventIntegrationTests
     }
 
     [Fact]
-    public void EventIntegration_ShouldNotCallLogger_WhenPropertyNestedClassIsNotModified_DetailIsDeep()
-    {
-        // Arrange
-        var logger = Mock.Of<ILogger<Net7.DeepWatchers.WithBlockNamespace>>();
-        var loggerMock = Mock.Get(logger);
-
-        using var @object = new Net7.DeepWatchers.WithBlockNamespace(logger);
-        
-        //Act
-        @object.Test2ThatDoNotModifies();
-
-        // Assert
-        Assert.Equal(Net7.DeepWatchers.WithBlockNamespace.OldName, @object.DummyClass.GetSetString);
-        Assert.Equal(Net7.DeepWatchers.WithBlockNamespace.OldNestedValue, @object.DummyClass.NestedClass);
-
-        loggerMock.Verify(
-            x => x.Log(
-                It.IsAny<LogLevel>(),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((v, t) => true),
-                It.IsAny<Exception?>(),
-                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), Times.Never);
-
-    }
-
-    [Fact]
     public void EventIntegration_ShouldCallLogger_WhenAPropertyIsModified_DetailIsNormal()
     {
         // Arrange
@@ -143,33 +90,6 @@ public class EventIntegrationTests
                 It.Is<It.IsAnyType>((v, t) => v.ToString() == $"Se ha escrito la propiedad GetSetString: Nuevo valor: {afterString} y antiguo valor: {beforeString}."),
                 It.IsAny<Exception?>(),
                 It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), Times.Once);
-
-
-    }
-
-    [Fact]
-    public void EventIntegration_ShouldNotCallLogger_WhenAPropertyIsNotModified_DetailIsNormal()
-    {
-        // Arrange
-        var logger = Mock.Of<ILogger<Net7.NormalWatchers.WithBlockNamespace>>();
-        var loggerMock = Mock.Get(logger);
-
-        using var @object = new Net7.NormalWatchers.WithBlockNamespace(logger);
-        
-        //Act
-        @object.TestThatDoNotModifies();
-
-        // Assert
-        Assert.Equal(Net7.DeepWatchers.WithBlockNamespace.OldName, @object.DummyClass.GetSetString);
-        Assert.Equal(Net7.DeepWatchers.WithBlockNamespace.OldNestedValue, @object.DummyClass.NestedClass);
-
-        loggerMock.Verify(
-            x => x.Log(
-                It.IsAny<LogLevel>(),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((v, t) => true),
-                It.IsAny<Exception?>(),
-                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), Times.Never);
 
 
     }
